5cc10cd111d278175cec30f595bc21c052e444b9
Vishal Gupta <vishalgupta7972@gmail.com>
2020-11-28 11:03:27 +0530

    Final project

--- flipbook.cpp
+++ flipbook.cpp
@@ -66,7 +66,6 @@ void process_base_stmt(FB_stmt *it)
 {
 		for(auto jt:*(((Base_stmt*)it)->properties))
 				{
-					cout << jt->pname << endl;
 					if(jt->pname == HEIGHT_T)
 						height = jt->val1;
 					else if(jt->pname == WIDTH_T)
@@ -76,7 +75,6 @@ void process_base_stmt(FB_stmt *it)
 					else if(jt->pname == TOTAL_FRAMES_T)
 						total_frames = jt->val1;
 				}
-	cout << height << " " << width << " " << frame_rate << " " << total_frames << endl;
 				to_draw.resize(total_frames);
 
 }
@@ -89,7 +87,6 @@ int frame_start = 1, frame_end = total_frames;
 				{
 					if(jt->pname == IMAGE_NAME_T)
 					{
-						cout << "img" << *(jt->str) << endl;
 						curr.image_name = jt->str;
 					}
 					else if(jt->pname == FRAME_START_T)
@@ -120,7 +117,6 @@ void process_line_stmt(FB_stmt *it)
 				{
 					if(jt->pname == IMAGE_NAME_T)
 					{
-						cout << "img" << *(jt->str) << endl;
 						curr.image_name = jt->str;
 					}
 					else if(jt->pname == FRAME_START_T)
@@ -161,7 +157,6 @@ void process_square_stmt(FB_stmt *it)
 				{
 					if(jt->pname == IMAGE_NAME_T)
 					{
-						cout << "img" << *(jt->str) << endl;
 						curr.image_name = jt->str;
 					}
 					else if(jt->pname == FRAME_START_T)
@@ -255,25 +250,18 @@ int main(int argc, char* argv[])
 			cout << "Frame " << i+1 << " objects: " << to_draw[i].size() << endl;
 			for(int j = 0; j < 3*width*height; j++)
 				buffer[j] = 255;
-			cout << "Here" << endl;
 			plt::xlim(0,width);
 			plt::ylim(0,height);
 			for(auto jt: to_draw[i])
 			{
 				if(jt.image_name == NULL)
 				{
-					cout << "No image" << endl;
 					continue;
 				}
-				cout << "here1" << endl;
-				cout << *(jt.image_name) << jt.start_posy << " " << jt.start_posx << endl;
-				cout << "here2" << endl;
 				int fig_width = 0, fig_height = 0;
 				char *filename = new char[jt.image_name->size()+1];
 				strcpy(filename, jt.image_name->c_str());
-				cout << "Read bmp: " << endl;
 				unsigned char* fig_data = readBMP(filename, fig_width, fig_height);				
-				cout << "Read bmp : " << fig_width << " " << fig_height << endl;
 				int temp_pos = 0;
 				for(int j = 0; j < fig_height; j++)
 				{
@@ -285,7 +273,6 @@ int main(int argc, char* argv[])
 				}
 				delete(fig_data);
 				//plt::imshow(fig_data, fig_height, fig_width, 3);
-				cout << "Copied to buffer " << endl;
 			}
 			plt::imshow(buffer,height,width,3);
 			plt::save(string("frame"+to_string(1000+i)).c_str());
--- tree.h
+++ tree.h
@@ -31,9 +31,9 @@ public:
 	string *str;
 	int val1;
 	int val2;
-	Property(int pname, int val1):pname(pname),val1(val1),val2(0) {cout << pname << " " << val1 << " " << val2 << endl;}
-	Property(int pname, string *str):pname(pname),str(str),val1(0),val2(0)  {cout << pname << " " << val1 << " " << val2 << endl;}
-	Property(int pname, int val1, int val2):pname(pname),val1(val1),val2(val2)  {cout << pname << " " << val1 << " " << val2 << endl;}
+	Property(int pname, int val1):pname(pname),val1(val1),val2(0) {}
+	Property(int pname, string *str):pname(pname),str(str),val1(0),val2(0)  {}
+	Property(int pname, int val1, int val2):pname(pname),val1(val1),val2(val2)  {}
 };
 
 class Base_stmt:public FB_stmt
